7305253782398dc3aefd1041983b54d8a112a4ad
Your Name <kelvinkiprono659@gmail.com>
2024-08-14 16:12:50 +0300

    Defining the User model

--- appointment-system/backend/models/User.js
+++ appointment-system/backend/models/User.js
@@ -1,2 +1,9 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../config/config');
+
+const User = sequelize.define('User', {
+    username: { type: DataTypes.STRING, unique: true, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
+    role: { type: DataTypes.STRING, allowNull: false }, // 'admin' or 'user'
+  });
+  
