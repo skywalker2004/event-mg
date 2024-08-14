86cb0f24ae75a5607d84e2b209250b129262a2b5
Your Name <kelvinkiprono659@gmail.com>
2024-08-14 15:44:13 +0300

    Configured server to listen on environment port or 5000

--- appointment-system/backend/index.js
+++ appointment-system/backend/index.js
@@ -6,3 +6,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 app.use(cors());
 app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
