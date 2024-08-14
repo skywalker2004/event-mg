66f0e80bcdf6ff570290a3cd68156db1b505066c
Your Name <kelvinkiprono659@gmail.com>
2024-08-14 15:42:51 +0300

    Initial setup of Express.js server and added CORS and JSON middleware

---
+++ appointment-system/backend/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+app.use(cors());
+app.use(express.json());
