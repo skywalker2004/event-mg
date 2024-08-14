4cfe3b091b8bb2415d30ee3a396e12a67385f53f
Your Name <kelvinkiprono659@gmail.com>
2024-08-14 16:31:33 +0300

    Creating the router

--- appointment-system/backend/routes/auth.js
+++ appointment-system/backend/routes/auth.js
@@ -2,3 +2,5 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+
+const router = express.Router();
