e976a4565ddf95bbe022457ba1bb4ff10beeac66
Your Name <kelvinkiprono659@gmail.com>
2024-08-14 16:41:55 +0300

    Defining the authenticate function and getting the token from the request header

--- appointment-system/backend/middleware/auth.js
+++ appointment-system/backend/middleware/auth.js
@@ -1,1 +1,6 @@
 const jwt = require('jsonwebtoken');
+
+const authenticate = (req, res, next) => {
+    const token = req.header('x-auth-token');
+    if (!token) return res.status(401).json({ error: 'No token, authorization denied' });
+  
