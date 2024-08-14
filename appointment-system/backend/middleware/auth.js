df97b1904f5ee6edbfb41ea5407df1311073e3e9
Your Name <kelvinkiprono659@gmail.com>
2024-08-14 16:44:53 +0300

    verifying the token

--- appointment-system/backend/middleware/auth.js
+++ appointment-system/backend/middleware/auth.js
@@ -4,3 +4,11 @@ const authenticate = (req, res, next) => {
     const token = req.header('x-auth-token');
     if (!token) return res.status(401).json({ error: 'No token, authorization denied' });
   
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+      } catch (e) {
+        res.status(400).json({ error: 'Token is not valid' });
+      }
+    };
