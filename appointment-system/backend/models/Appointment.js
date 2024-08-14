0b606bfe7ced887f7105c603d6022cca2d63aa9a
Your Name <kelvinkiprono659@gmail.com>
2024-08-14 16:19:29 +0300

    Defining the Appointment model

--- appointment-system/backend/models/Appointment.js
+++ appointment-system/backend/models/Appointment.js
@@ -1,2 +1,8 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../config/config');
+
+const Appointment = sequelize.define('Appointment', {
+    userId: { type: DataTypes.INTEGER, allowNull: false },
+    date: { type: DataTypes.DATE, allowNull: false },
+    amount: { type: DataTypes.DECIMAL, allowNull: false },
+  });
